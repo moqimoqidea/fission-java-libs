23959611e32e4cd5fa4df7562ce1ae2eecb4927c
Vishal Biyani <vishal@infracloud.io>
2018-04-30 14:33:56 +0530

    Draft commit of the Fission java core lib

---
+++ fission-java-core/src/main/java/io/fission/FissionFunction.java
@@ -0,0 +1,10 @@
+package io.fission;
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+
+public interface FissionFunction<RequestEntity, ResponseEntity> {
+	
+	org.springframework.http.ResponseEntity call(org.springframework.http.RequestEntity req, FissionContext context); 
+
+}
